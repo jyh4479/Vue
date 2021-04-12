e20c5fe2bf67dd5d5ad8f0ed63b4df4f2c3fdca8
Jeong yong hoon <jyh.dev@gmail.com>
2021-04-12 17:27:35 +0900

    Vue practice

--- Vue practice/vue-app/src/App.vue
+++ Vue practice/vue-app/src/App.vue
@@ -1,17 +1,41 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>{{num}}</div>
+    <HelloWorld msg="Welcome to Your Vue.js App" v-bind:title='num'/>
+    <div>
+      <button v-on:click="testFunction">get Data</button>
+    </div>  
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
+//import ComponentsTest from './components/ComponentsTest.vue'
 
 export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  data: function(){
+    return{
+      num: "100"
+    }
+  },
+  methods: {
+    testFunction: function(){
+      var vm=this;
+      axios.get('http://localhost:8000/vuetest')
+      .then(function(response){
+        console.log(response.data);
+        vm.num=response.data;
+      })
+      .catch(function(error){
+        console.log(error);
+      });
+    }
   }
 }
 </script>
